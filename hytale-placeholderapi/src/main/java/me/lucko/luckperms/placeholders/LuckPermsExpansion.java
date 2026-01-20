50de93b53a5fe7929b99c211e0753a707617d6cf
PiggyPiglet <noreply@piggypiglet.me>
2026-01-20 21:23:21 +0800

    Add Hytale Support

---
+++ hytale-placeholderapi/src/main/java/me/lucko/luckperms/placeholders/LuckPermsExpansion.java
@@ -0,0 +1,100 @@
+/*
+ * This file is part of LuckPerms, licensed under the MIT License.
+ *
+ *  Copyright (c) lucko (Luck) <luck@lucko.me>
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package me.lucko.luckperms.placeholders;
+
+import com.hypixel.hytale.common.plugin.PluginIdentifier;
+import com.hypixel.hytale.server.core.HytaleServer;
+import at.helpch.placeholderapi.PlaceholderAPIPlugin;
+import at.helpch.placeholderapi.expansion.PlaceholderExpansion;
+import at.helpch.placeholderapi.util.TimeUtil;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+
+/**
+ * PlaceholderAPI Expansion for LuckPerms, implemented using the LuckPerms API.
+ */
+public class LuckPermsExpansion extends PlaceholderExpansion implements PlaceholderPlatform {
+    private static final String IDENTIFIER = "hluckperms";
+    private static final String PLUGIN_NAME = "LuckPerms:LuckPerms";
+    private static final String AUTHOR = "Luck";
+    private static final String VERSION = "5.4-R2";
+
+    private LPPlaceholderProvider provider;
+
+    @Override
+    public boolean canRegister() {
+        return HytaleServer.get().getPluginManager().getPlugin(PluginIdentifier.fromString(PLUGIN_NAME)) != null;
+    }
+
+    @Override
+    public boolean register() {
+        if (!canRegister()) {
+            return false;
+        }
+
+//        LuckPerms luckPerms = Bukkit.getServicesManager().getRegistration(LuckPerms.class).getProvider();
+        final LuckPerms luckPerms = LuckPermsProvider.get();
+
+        this.provider = new LPPlaceholderProvider(this, luckPerms);
+        return super.register();
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, String identifier) {
+        if (player == null || this.provider == null) {
+            return "";
+        }
+
+        return this.provider.onPlaceholderRequest(player.getPlayerRef(), player.getUuid(), identifier);
+    }
+
+    @Override
+    public String formatBoolean(boolean b) {
+        return b ? PlaceholderAPIPlugin.instance().configManager().config().booleanValue().trueValue() : PlaceholderAPIPlugin.instance().configManager().config().booleanValue().falseValue();
+    }
+
+    @Override
+    public String getIdentifier() {
+        return IDENTIFIER;
+    }
+
+    @Override
+    public String getPlugin() {
+        return PLUGIN_NAME;
+    }
+
+    @Override
+    public String getAuthor() {
+        return AUTHOR;
+    }
+
+    @Override
+    public String getVersion() {
+        return VERSION;
+    }
+
+}
